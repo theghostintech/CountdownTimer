141f01d117a412e7d687bff55faf4b2402fbd3ab
theghostintech <theghostintech@gmail.com>
2023-04-07 12:29:07 -0700

    a test

---
+++ SomethingNew.swift
@@ -0,0 +1,20 @@
+//
+//  SomethingNew.swift
+//  CountdownTimer
+//
+//  Created by Joshua Birdine on 4/7/23.
+//
+
+import SwiftUI
+
+struct SomethingNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SomethingNew_Previews: PreviewProvider {
+    static var previews: some View {
+        SomethingNew()
+    }
+}
