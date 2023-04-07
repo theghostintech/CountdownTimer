f8817a80fd18e50c836e809078074fb40d315e2e
theghostintech <theghostintech@gmail.com>
2023-04-07 12:27:29 -0700

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Joshua Birdine on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
